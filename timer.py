b015c578612af4cc1b6d330048a47e9c332ab596
Leonidas-source <66934329+Leonidas-source@users.noreply.github.com>
2022-02-21 19:36:29 +0700

    Add files via upload

---
+++ timer.py
@@ -0,0 +1,36 @@
+import time
+import os
+timer = int(input("Set your time in minutes: "))
+if timer == 1:
+    a = 60
+    while a != 0:
+        a = a - 1
+        os.system("clear")
+        print(str(a) + " seconds left")
+        time.sleep(1)
+    os.system("brave https://www.youtube.com/watch?v=wNH3UVpYSSs&list=RDMM&index=24")
+elif timer > 1:
+    timer = timer - 1
+    a = 60
+    os.system("clear")
+    print(str(timer) + " minutes " + str(a) + " seconds left")
+    while timer != 0:
+        while a != 0:
+            a = a - 1
+            os.system("clear")
+            print(str(timer) + " minutes " + str(a) + " seconds left")
+            time.sleep(1)
+        while a == 0:
+            a = 60
+            timer = timer - 1
+            os.system("clear")
+            print(str(timer) + " minutes " + str(a) + " seconds left")
+            time.sleep(1)
+    if timer == 0:
+        a = 60
+        while a != 0:
+            a = a - 1
+            os.system("clear")
+            print(str(a) + " seconds left")
+            time.sleep(1)
+        os.system("brave https://www.youtube.com/watch?v=wNH3UVpYSSs&list=RDMM&index=24")
